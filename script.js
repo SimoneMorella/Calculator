bc1d1e3eebaaed83713c3fbb7f5b97fd3d091b23
SimoneMorella <simonemorella21@gmail.com>
2023-12-18 04:14:36 +0100

    finished js and bugfixed, just need to adjust responsivness

--- script.js
+++ script.js
@@ -2,12 +2,33 @@ let resultDisplay = document.querySelector(".resultdisplay");
 let upperDisplay = document.querySelector(".upperdisplay");
 let numbersBtn = document.querySelectorAll(".btn.numbtn");
 let opBtn = document.querySelectorAll(".btn.opbtn");
-let equalBtn = document.querySelector("#equal");
+const equalBtn = document.querySelector("#equal");
 const allClearBtn = document.querySelector("#allClear");
 const clearBtn = document.querySelector("#clear");
 const decimalBtn = document.querySelector("#decimal");
+const zeroBtn = document.querySelector("#zero");
+const changeSignBtn = document.querySelector("#changeSign");
 let opArray = ["+", "-", "×", "÷"];
 let calculationResult = false;
+
+decimalBtn.addEventListener("click", (e) => {
+    if (resultDisplay.textContent === '0') {
+        resultDisplay.textContent = "0.";
+        decimalBtn.disabled = true;
+    }
+    else if (resultDisplay.textContent.includes(".")) {
+        decimalBtn.disabled = true;
+    }
+    else {
+        resultDisplay.textContent += ".";
+    }
+})
+
+zeroBtn.addEventListener("click", (e) => {
+    if (resultDisplay.textContent !== '0' && resultDisplay.textContent.length < 8) {
+        resultDisplay.textContent += "0";
+    }
+})
 
 numbersBtn.forEach((btn) => {
     btn.addEventListener("click", (e) => {
@@ -15,17 +36,24 @@ numbersBtn.forEach((btn) => {
             clearAll()
             calculationResult = false;
         }
-        resultDisplay.textContent += e.target.textContent;
+        if (e.target.textContent !== "0" && e.target.textContent !== "." && resultDisplay.textContent.length < 8) {
+            if (resultDisplay.textContent === "0"){
+                resultDisplay.textContent = "";
+            }
+            resultDisplay.textContent += e.target.textContent;
+        }
     })
 })
 
 opBtn.forEach((btn) => {
     btn.addEventListener("click", (e) => {
         if (e.target.textContent !== "=" && upperDisplay.textContent === '') {
+            decimalBtn.disabled = false;
             upperDisplay.textContent = `${resultDisplay.textContent} ${e.target.textContent} `;
             resultDisplay.textContent = '';
         }
         else if (e.target.textContent !== "=") {
+            decimalBtn.disabled = false;
             equalBtn.click();
             upperDisplay.textContent = `${resultDisplay.textContent} ${e.target.textContent} `;
             if (calculationResult) {
@@ -37,20 +65,24 @@ opBtn.forEach((btn) => {
 })
 
 equalBtn.addEventListener("click", () => {
-    upperDisplay.textContent += resultDisplay.textContent;
-    let equationArray = upperDisplay.textContent.split(" ");
-    let calculation = {
-        firstNumber : equationArray[0],
-        operationSymbol : equationArray[1],
-        secondNumber : equationArray[2],
+    if (!calculationResult) {
+        upperDisplay.textContent += resultDisplay.textContent;
+        let equationArray = upperDisplay.textContent.split(" ");
+        let calculation = {
+            firstNumber : equationArray[0],
+            operationSymbol : equationArray[1],
+            secondNumber : equationArray[2],
+        }
+        let equationResult = operate(calculation.operationSymbol, calculation.firstNumber, calculation.secondNumber);
+        resultDisplay.textContent = Math.round(equationResult * 1000) / 1000; // best way to round at 4th digit decimal
+        calculationResult = true;
     }
-    resultDisplay.textContent = operate(calculation.operationSymbol, calculation.firstNumber, calculation.secondNumber);
-    calculationResult = true;
 })
 
 clearBtn.addEventListener("click", () => {
-    // con calculationResult si non cancella, con no cancella
-    upperDisplay.textContent = upperDisplay.textContent.slice(0, -1);
+    if (!calculationResult) {
+        resultDisplay.textContent = resultDisplay.textContent.slice(0, -1);
+    }
 })
 
 allClearBtn.addEventListener("click", () => {
@@ -59,13 +91,23 @@ allClearBtn.addEventListener("click", () => {
 
 function clearAll() {
     upperDisplay.textContent = "";
-    resultDisplay.textContent = "";
+    resultDisplay.textContent = "0";
+    decimalBtn.disabled = false;
 }
+
+changeSignBtn.addEventListener("click", () => {
+    if (resultDisplay.textContent) {
+        resultDisplay.textContent = changesign(resultDisplay.textContent);
+    }
+})
 
 function operate(op, a, b) {
     let result;
     if (b === undefined) {
         return a;
+    }
+    if (op === "÷" && b === '0') {
+        alert("You can't divide by 0! Please restart the page!");
     }
     switch(op) {
         case "+":
@@ -81,9 +123,9 @@ function operate(op, a, b) {
             result = division(a,b);
             break;
     }
+
     return result;
 }
-
 
 function addition (a,b) {
     return +a + +b;
@@ -98,9 +140,6 @@ function multiply (a, b) {
 }
 
 function division (a, b) {
-    if (b === 0) {
-        return "Error: Division by 0";
-    }
     return +a / +b;
 }
 
